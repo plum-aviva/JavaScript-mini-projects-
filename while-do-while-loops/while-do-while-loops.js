9a98c8f1cd2d3dadc89f8d89a9ddd714ec15da7b
Plum Bristow <plum.bristow@aviva.com>
2025-02-12 08:21:33 +0000

    adding while-loops and do while loops

---
+++ while-do-while-loops/while-do-while-loops.js
@@ -0,0 +1,38 @@
+// this idea could be used for spelling tests? Help me learn new words? 
+// while loop that will only accept word cat 
+let spelling1 = "";
+
+
+document.getElementById("whileLoop").onclick = function () {
+    while (spelling1 !== "cat") {
+        spelling1 = window.prompt(`enter spelling one`);
+
+    }
+    document.getElementById("rightAnswer").innerHTML = "Correct - cat"
+}
+// will only accept the word dog 
+let spelling2 = "";
+// let letterArray = [...spelling2]
+
+
+let animal;
+document.getElementById("doWhileLoop1").onclick = function () {
+    do {
+        animal = window.prompt(`Enter animal name`);
+        document.getElementById("wrongAnswer1").innerHTML = "right Answer - it's a " + animal;
+    } while (animal !== "dog")
+
+
+}
+
+//was playing with the idea of changing number to a string - but didn't make sense using this method will build something seperate
+
+let input = "";
+
+document.getElementById("changeToString").onclick = function () {
+    while (true) {
+        input = window.prompt("enter a number");
+        if (!isNaN(input))
+            break;
+    }
+}
